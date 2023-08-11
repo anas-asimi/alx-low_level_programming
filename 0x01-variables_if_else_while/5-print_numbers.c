95362a9136959fe58f10cb3f1031ce5114cad0e0
Anas Asimi <anas2020cr7@gmail.com>
2023-08-11 21:54:31 +0100

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int number;
+
+    for (number = 0; number < 10; number++)
+        printf("%i\n", number);
+
+    return (0);
+}
