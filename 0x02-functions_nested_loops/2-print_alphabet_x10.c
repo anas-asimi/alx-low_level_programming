065c5148b428907ddd87c35e20e9e7c17d0d67ac
Anas Asimi <anas2020cr7@gmail.com>
2023-08-15 20:04:11 +0100

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints 10 times the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
