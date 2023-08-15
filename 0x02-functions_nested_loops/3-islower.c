43abe65c72d2e8497de4b542109af41545ccb7ee
Anas Asimi <anas2020cr7@gmail.com>
2023-08-15 20:13:25 +0100

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase.
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	return (0);
+}
