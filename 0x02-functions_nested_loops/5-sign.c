b102cb85bc65df3eb674fe5d60338d5c3d3dcd6c
Anas Asimi <anas2020cr7@gmail.com>
2023-08-15 20:27:56 +0100

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,11 +8,6 @@
 
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
 	if (n > 0)
 	{
 		_putchar('+');
@@ -22,5 +17,10 @@ int print_sign(int n)
 	{
 		_putchar('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
 	}
 }
