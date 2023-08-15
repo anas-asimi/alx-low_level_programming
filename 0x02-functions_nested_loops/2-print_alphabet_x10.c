88ce6b5fb87a7ae4c0970901e8b66fbd1fd76c88
Anas Asimi <anas2020cr7@gmail.com>
2023-08-15 20:04:52 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,10 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	char c;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
+		print_alphabet()
 	}
 	_putchar('\n');
 }
