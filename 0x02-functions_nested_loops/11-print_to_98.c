2b9eb55cd1fb14a1801eb287d34dd8f7b5927c42
Anas Asimi <anas2020cr7@gmail.com>
2023-08-16 18:26:06 +0100

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,37 @@
  */
 void print_to_98(int n)
 {
-	int count;
+	int x;
+	int first_digit;
+	int second_digit;
+	int third_digit;
 
-	if (n > 98)
-		for (count = n; count > 98; --count)
-			printf("%d, ", count);
-	else
-		for (count = n; count < 98; ++count)
-			printf("%d, ", count);
-	printf("98\n");
+	while (1)
+	{
+		x = n >= 0 ? n : -n;
+		first_digit = (x / 100) % 10;
+		second_digit = (x / 10) % 10;
+		third_digit = (x / 1) % 10;
+		if (x >= 100)
+		{
+			if (x < 1000 && n < 0)
+				_putchar('-');
+			_putchar(first_digit + '0');
+		}
+		if (x >= 10)
+		{
+			if (x < 100 && n < 0)
+				_putchar('-');
+			_putchar(second_digit + '0');
+		}
+		if (x < 10 && n < 0)
+			_putchar('-');
+		_putchar(third_digit + '0');
+		if (n == 98)
+			break;
+		_putchar(',');
+		_putchar(' ');
+		n = (n < 98) ? (n + 1) : (n - 1);
+	}
+	_putchar('\n');
 }
