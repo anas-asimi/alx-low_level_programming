0c30b574a91869b672e76cea7b4961c63888b206
Anas Asimi <anas2020cr7@gmail.com>
2023-08-16 18:09:28 +0100

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,14 +16,9 @@ void print_to_98(int n)
 	{
 		isPositive = n >= 0;
 		if (isPositive)
-		{
 			x = n;
-		}
 		else
-		{
 			x = -n;
-		}
-
 		first_digit = (x / 100) % 10;
 		second_digit = (x / 10) % 10;
 		third_digit = (x / 1) % 10;
@@ -31,45 +26,31 @@ void print_to_98(int n)
 		if (x >= 100)
 		{
 			if (x < 1000 && !isPositive)
-			{
 				_putchar('-');
-			}
 			_putchar(first_digit + '0');
 		}
 
 		if (x >= 10)
 		{
 			if (x < 100 && !isPositive)
-			{
 				_putchar('-');
-			}
 			_putchar(second_digit + '0');
 		}
 
 		if (x < 10 && !isPositive)
-		{
 			_putchar('-');
-		}
-
 		_putchar(third_digit + '0');
 		if (n != 98)
 		{
 			_putchar(',');
 			_putchar(' ');
 			if (n < 98)
-			{
 				n++;
-			}
 			else if (n > 98)
-			{
 				n--;
-			}
 		}
-		else 
-		{
+		else
 			break;
-		}
 	}
-
 	_putchar('\n');
 }
