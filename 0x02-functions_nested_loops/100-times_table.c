ff04ec903c3a1348969cfdafce285cb2a6715ab3
Anas Asimi <anas2020cr7@gmail.com>
2023-08-16 17:01:33 +0100

    commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table,
+ * inorder separated by comma, followed by a space
+ * @n: is used as int to pass argument
+ */
+
+void print_times_table(int n)
+{
+	int x;
+	int y;
+	int z;
+
+	if (n < 0 || n > 15)
+	{
+		return (0);
+	}
+
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (y > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (z >= 100)
+				{
+					_putchar(z / 100 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				if (z >= 10)
+				{
+					_putchar(z / 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar(z % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
