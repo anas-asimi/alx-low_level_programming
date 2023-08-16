dcf1c4220251c61de4a89667a47432880d923750
Anas Asimi <anas2020cr7@gmail.com>
2023-08-16 17:53:27 +0100

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
  * print_to_98 - prints all natural numbers from input to 98,
  * inorder separated by comma, followed by a space
@@ -7,16 +6,72 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	int x;
+	int first_digit;
+	int second_digit;
+	int third_digit;
+	int isPositive;
+
+	do
 	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
-	}
-	else
-	{
-		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
-	}
+		isPositive = n >= 0;
+		if (isPositive)
+		{
+			x = n;
+		}
+		else
+		{
+			x = -n;
+		}
+
+		first_digit = (x / 100) % 10;
+		second_digit = (x / 10) % 10;
+		third_digit = (x / 1) % 10;
+
+		if (x >= 100)
+		{
+			if (!isPositive)
+			{
+				_putchar('-');
+			}
+			_putchar(first_digit + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+
+		if (x >= 10)
+		{
+			if (!isPositive)
+			{
+				_putchar('-');
+			}
+			_putchar(second_digit + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+
+		if (x < 10 && !isPositive)
+		{
+			_putchar('-');
+		}
+
+		_putchar(third_digit + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+			if (n < 98)
+			{
+				n++;
+			}
+			else if (n > 98)
+			{
+				n--;
+			}
+		}
+	} while (n != 98);
 }
