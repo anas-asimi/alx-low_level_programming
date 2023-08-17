94bd6772bfb2c7da294c49c41f8c136e91f4800a
Anas Asimi <anas2020cr7@gmail.com>
2023-08-17 19:34:59 +0100

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,26 +4,21 @@
  * print_diagonal - draws a diagonal line
  * @n: takes in an integer
  */
+
 void print_diagonal(int n)
 {
 	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
-	
-	for (i = 0; i < n; i++)
+
+	for (i = 1; i <= n; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 1; j < i; j++)
 		{
-			if (i == j)
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(' ');
-			}
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
