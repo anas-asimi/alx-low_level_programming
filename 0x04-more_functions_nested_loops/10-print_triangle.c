2f5e3a422248fb1ebe3a03f67197616c8de6d64e
Anas Asimi <anas2020cr7@gmail.com>
2023-08-17 20:38:08 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: takes in an integer for size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int row, column, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size - row; column++)
+			_putchar(' ');
+		for (k = 0; k <= row; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
