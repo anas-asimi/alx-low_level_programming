652ca5f765931032fbe765272e3553ae3ce3ffe4
Anas Asimi <anas2020cr7@gmail.com>
2023-08-17 18:57:41 +0100

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,12 @@ void print_most_numbers(void)
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
+	{
 		if (c == '2' || c == '4')
 		{
 			continue;
 		}
-	_putchar(c);
-	_putchar('\n');
+		_putchar(c);
+		_putchar('\n');
+	}
 }
