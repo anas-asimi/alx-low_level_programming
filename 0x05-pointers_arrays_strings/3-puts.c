ede6a1d4a7112a439543fce2a1d6ed4b4ec889ba
Anas Asimi <anas2020cr7@gmail.com>
2023-08-22 12:10:07 +0100

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - print a string followed by a new line to stdout.
- * @str: the input string to be printed
- * Return: Always void.
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
  */
 
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[i]);
+		i++;
 	}
-
 	_putchar('\n');
 }
