be925fd4f16243b02999ae3550a1e7ff06da5b2e
Anas Asimi <anas2020cr7@gmail.com>
2023-08-22 15:13:05 +0100

    commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,8 @@ void puts2(char *str)
 {
 	int i;
 
-	for (; str[i] != '\0'; i++)
-	if (i % 2 == 0)
-		_putchar(str[i]);
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	_putchar('\n');
 }
