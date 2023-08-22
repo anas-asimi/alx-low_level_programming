2edf0e29e631fed0cf339ff74cb4f4d480eafe8b
Anas Asimi <anas2020cr7@gmail.com>
2023-08-22 15:16:20 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - Prints element of array
+ * @a: array name
+ * @n: elements of the array
+ * Return: Always not.
+ */
+
+void Print_array(int *a, int n) /* void */
+{
+	for (int i = 0; i < n; i++)
+	{
+		_putchar(a[i] + '0');
+		if (i < n - 1)
+		{
+			_putchar(",");
+			_putchar(" ");
+		}
+	}
+	printf("\n");
+}
