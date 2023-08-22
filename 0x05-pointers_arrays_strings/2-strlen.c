2eaa3c8711845b6b1aeecaf68753ec185bec0e4c
Anas Asimi <anas2020cr7@gmail.com>
2023-08-22 12:02:03 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the function of a string.
+ * @s: value to evaluate
+ * Return: Always void.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (1)
+	{
+		if (*(s + length) == '\0')
+			break;
+		else
+			length++;
+	}
+	return (length);
+}
