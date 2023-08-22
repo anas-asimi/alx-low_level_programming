20d3dd64df74a5a18a6d0fe69c6c6e98d49e81d5
Anas Asimi <anas2020cr7@gmail.com>
2023-08-22 12:21:42 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev -> printing a string in reverse
+ * @s: the string to be printed in rev
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+
+	for (i; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
