b7784ded61d5a343df750509483ffa30f42758ed
Anas Asimi <anas2020cr7@gmail.com>
2023-08-22 11:55:31 +0100

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter 
- * and updates the value it points to to 98.
- *
- * Return: Void.
+ * reset_to_98 - update the value by pointer
+ * @n: variable to update
+ * Return: Always void.
  */
 
 void reset_to_98(int *n)
