69f5393d4d29d61d020026b5f49e898080e5d201
Anas Asimi <anas2020cr7@gmail.com>
2023-08-24 21:42:26 +0100

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,10 +13,10 @@ void print_number(int n)
 		n = -n;
 	}
 	if (n > 1000)
-		print_number((n / 1000) % 10);
+		_putchar((n / 1000) % 10 + '0');
 	if (n > 100)
-		print_number((n / 100) % 10);
+		_putchar((n / 100) % 10 + '0');
 	if (n > 10)
-		print_number((n / 10) % 10);
-	_putchar(n % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+	_putchar((n / 1) % 10 + '0');
 }
