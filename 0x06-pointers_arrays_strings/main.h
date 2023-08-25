cb26ee2e665a9b650416df4b019eea97d0a37cf9
Anas Asimi <anas2020cr7@gmail.com>
2023-08-25 10:26:18 +0100

    commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,6 @@ char *cap_string(char *s);
 char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int n);
+void print_buffer(char *b, int size);
+
 #endif
