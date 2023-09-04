288623af20606a9eb61fda34f1a9d513ac61a52e
Anas Asimi <anas2020cr7@gmail.com>
2023-09-04 22:50:30 +0100

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,44 +1,57 @@
 #include "main.h"
 #include <stdlib.h>
 
-void util(char **, char *);
-void create_word(char **, char *, int, int, int);
-
 /**
  * strtow - splits a string into words.
  * @str: the string
  *
  * Return: returns a pointer to an array of strings (words)
  */
+
 char **strtow(char *str)
 {
-	    int i = 0;
-    int numberOfWords = 0;
-    int totalLenght = 0;
-    char string[] = "     123    456 789 ";
+	int j = 0, i = 0;
+	int **array;
+	int numberOfWords = 0;
+	int totalLenght = 0;
+	char string[] = "     123    456 789 ";
 
-    /* calcule number of words and caracters */
-    while(string[i])
-    {
-        printf("checking : %c\n",string[i]);
-        if(string[i] ==' ')
-        {
-            printf("its white space\n");
-            i++;
-            continue;
-        }
-        
-        printf("tis not white space\n");
-        numberOfWords++;
-        while(string[i] !=' ' && string[i])
-        {
-            totalLenght++;
-            i++;
-            
-        }
+	/* calcule number of words and caracters */
+	while (string[i])
+	{
+		printf("checking : %c\n", string[i]);
+		if (string[i] == ' ')
+		{
+			i++;
+			continue;
+		}
 
-    }
-    printf("found : %d words\n",numberOfWords);
-    printf("lenght : %d\n",totalLenght);
+		numberOfWords++;
+		while (string[i] != ' ' && string[i])
+		{
+			totalLenght++;
+			i++;
+		}
+	}
+	printf("found : %d words\n", numberOfWords);
+	printf("lenght : %d\n", totalLenght);
 
+	/* allocate needed memory */
+	array = malloc(sizeof(char) * (numberOfWords + totalLenght));
+	i = 0;
+	while (string[i])
+	{
+		if (string[i] == ' ')
+		{
+			i++;
+			continue;
+		}
+
+		while (string[i] != ' ' && string[i])
+		{
+			array[j] = string[i];
+			j++;
+			i++;
+		}
+	}
 }
