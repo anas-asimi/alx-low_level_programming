58de00348e6b09ce910d617902fc956d0ec252e4
Anas Asimi <anas2020cr7@gmail.com>
2023-09-05 00:02:18 +0100

    test file

---
+++ 0x0B-malloc_free/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	return (0);
+}
+// create array of array of 2 int
+// where the first int is first index of word
+// and the last int is last index of word
+
+// store the array length in int
+// allocate memory for the array
+
+// for every item in array allocate array of char and fill it
