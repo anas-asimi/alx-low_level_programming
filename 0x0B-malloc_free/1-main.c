f01dcba03602f8fa9b170fdad99890fa78a5fa2c
Anas Asimi <anas2020cr7@gmail.com>
2023-09-04 11:48:56 +0100

    commit

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = _strdup("");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	s = _strdup("ALX SE");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
