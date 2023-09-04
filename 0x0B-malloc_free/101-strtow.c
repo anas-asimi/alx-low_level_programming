b99bb10bebc0de9161edc65998f62687a7152395
Anas Asimi <anas2020cr7@gmail.com>
2023-09-04 22:38:47 +0100

    commit

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+void util(char **, char *);
+void create_word(char **, char *, int, int, int);
+
+/**
+ * strtow - splits a string into words.
+ * @str: the string
+ *
+ * Return: returns a pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	
+}
