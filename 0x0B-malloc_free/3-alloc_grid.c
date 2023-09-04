f42c8f46800812aa8338d6d35f6dc2848c6f9f0a
Anas Asimi <anas2020cr7@gmail.com>
2023-09-04 14:46:46 +0100

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,13 @@ int **alloc_grid(int width, int height)
 	{
 		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			i--;
+			for (; i >= 0; i--)
+				free(array[i]);
+			free(array);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			array[i][j] = 0;
 	}
