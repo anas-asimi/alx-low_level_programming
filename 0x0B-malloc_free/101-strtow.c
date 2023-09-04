d2f032a27c6499f24892d560f0afc18f205166f2
Anas Asimi <96184837+anas-asimi@users.noreply.github.com>
2023-09-04 22:40:28 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,5 +12,33 @@ void create_word(char **, char *, int, int, int);
  */
 char **strtow(char *str)
 {
-	
+	    int i = 0;
+    int numberOfWords = 0;
+    int totalLenght = 0;
+    char string[] = "     123    456 789 ";
+
+    /* calcule number of words and caracters */
+    while(string[i])
+    {
+        printf("checking : %c\n",string[i]);
+        if(string[i] ==' ')
+        {
+            printf("its white space\n");
+            i++;
+            continue;
+        }
+        
+        printf("tis not white space\n");
+        numberOfWords++;
+        while(string[i] !=' ' && string[i])
+        {
+            totalLenght++;
+            i++;
+            
+        }
+
+    }
+    printf("found : %d words\n",numberOfWords);
+    printf("lenght : %d\n",totalLenght);
+
 }
