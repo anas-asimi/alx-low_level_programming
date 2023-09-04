e2c60aac8705fc2901261d693d10f6dcc6ec571c
Anas Asimi <anas2020cr7@gmail.com>
2023-09-04 12:16:05 +0100

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,26 +14,22 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2;
+	int i, j, len1, len2;
 	char *result;
 
+	i = j = len1 = len2 = 0;
 	if (s1 != NULL)
 		while (s1[len1] != '\0')
 			len1++;
-
 	if (s2 != NULL)
 		while (s2[len2] != '\0')
 			len2++;
-
 	result = malloc((sizeof(char) * (len1 + len2)) + 1);
 	if (result == NULL)
 		return (NULL);
-
-	for (i = 0; i <= len1 + len2; i++){
-		if (i < len1)
-			result[i] = s1[i];
-		else
-			result[i] = s2[i - len1];
-	}
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j <= len2; j++, i++)
+		result[i] = s2[j];
 	return (result);
 }
