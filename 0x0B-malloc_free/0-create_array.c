a83e5021bd638afdc91f448c63e8b76c04f60482
Anas Asimi <anas2020cr7@gmail.com>
2023-09-04 11:44:50 +0100

    commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars.
+ * @size: the size of the array
+ * @c: initial value
+ *
+ * Return: Pointer or NULL.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
