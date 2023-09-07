2e948a34ad9aef73fca26de0940a9960f2fddd7f
Anas Asimi <anas2020cr7@gmail.com>
2023-09-07 22:22:08 +0100

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: size to allocate memory.
+ *
+ * Return: a pointer with the memory location.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
