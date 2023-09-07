ea196f713bd532867265eef66ac05cc0488e7ad3
Anas Asimi <anas2020cr7@gmail.com>
2023-09-07 23:01:13 +0100

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,22 +18,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1[len1])
 		len1++;
-
 	while (s2[len2])
 		len2++;
-
 	s = malloc(sizeof(char) * (len1 + n + 1));
-
 	if (s == NULL)
 		return (NULL);
-
 	for (i = 0; i < len1; i++)
 		s[i] = s1[i];
-
 	for (i = 0; i < n; i++)
 		s[i + len1] = s2[i];
-
 	s[len1 + n] = '\0';
-
 	return (s);
 }
