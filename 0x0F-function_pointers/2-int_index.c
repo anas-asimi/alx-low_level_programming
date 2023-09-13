95af497413b5a565c4336d62f8761578cdfcdbc6
Anas Asimi <anas2020cr7@gmail.com>
2023-09-13 16:43:51 +0100

    commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+/**
+ * int_index - a function that searches for an integer.
+ * @array: array
+ * @size: size
+ * @cmp: function to executes
+ *
+ * Return: void.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int result;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		result = (*cmp)(array[i]);
+		if (result)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
