1dd65804a62652b62a69fe83fc7fa35bac4d387c
Anas Asimi <anas2020cr7@gmail.com>
2023-09-16 00:29:56 +0100

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -81,11 +81,7 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				put = va_arg(print_all, char*), e = 1;
-				if (!put)
-				{
-					printf("(nil)");
-					break;
-				}
+
 				printf("%s", put);
 				break;
 		} x++;
