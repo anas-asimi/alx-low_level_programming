1d6c3ca234d37d1bfeb97b09444ca8ee6d9c4c97
Anas Asimi <anas2020cr7@gmail.com>
2023-09-16 00:36:55 +0100

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,48 +3,6 @@
 #include <stdio.h>
 
 /**
- * chk_char - prints the char character
- * @list: the type
- * Return: nothing
- */
-void chk_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-/**
- * chk_int - prints the int
- * @list: the type
- * Return: nothing
- */
-void chk_int(va_list list)
-{
-	printf("%i", va_arg(list, int));
-}
-/**
- * chk_float - prints the float
- * @list: the type
- * Return: nothing
- */
-void chk_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-/**
- * chk_string - prints the string
- * @list: the type
- * Return: nothing
- */
-void chk_string(va_list list)
-{
-	char *str;
-
-	str = va_arg(list, char *);
-	if (str == NULL)
-		str = "(nil)";
-
-	printf("%s", str);
-}
-/**
  * print_all - prints anything
  * @format: list of types of arguments passed to function
  * Return: nothing
