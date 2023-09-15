b3d529d82095ef88d415a789992e27c9370f4d84
Anas Asimi <anas2020cr7@gmail.com>
2023-09-16 00:05:00 +0100

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 	int result;
 
-	if (size <= 0 && !array && !cmp)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
 
 	while (i < size)
