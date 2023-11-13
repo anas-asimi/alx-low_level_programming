befe5846f0e2ebb2cf523fa0afb538251033e2d8
Anas Asimi <anas2020cr7@gmail.com>
2023-11-14 00:13:16 +0100

    alx task

---
+++ 0x18-dynamic_libraries/functions/strcpy.c
@@ -0,0 +1,19 @@
+#include "../main.h"
+
+/**
+ *_strcpy - copy string including the terminating null byte
+ *@dest: destination of string
+ *@src: source of string
+ *
+ *Return: the value dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+for (i = 0; src[i]; i++)
+dest[i] = src[i];
+dest[i] = src[i];
+return (dest);
+}
